b4709637f634f493fc214a4bd007c2f82ea78922
chaoyixue <chaoyi.xue@gmail.com>
2021-03-11 11:29:20 +0100

    Update crop_images.py

--- crop_images.py
+++ crop_images.py
@@ -4,42 +4,37 @@ import os
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 from tqdm import tqdm
+import cv2
 
 
-def crop_levre_images(filepath, longeur,height_bot, height_top, width_left, width_right, output_name):
+def crop_images(filepath, longeur, height_bot, height_top, width_left, width_right, output_name):
     """
+    crop all the images in a folder and save the images as npy file
 
     :param width_right:
     :param width_left:
     :param height_top:
     :param height_bot:
-    :param filepath:
-    :param longeur:
-    :param output_name:
+    :param filepath: the folder that contains images
+    :param longeur: the number of images in the folder
+    :param output_name: the name of the output file npy
     :return:
     """
     data_npy_list = []
     for index_image in tqdm(range(longeur)):
-        mylevre = mpimg.imread(filepath + str(index_image+1) + ".bmp")
-        mylevre = mylevre[:]
-    pass
+        myimg = mpimg.imread(filepath + str(index_image+1) + ".bmp")
+        myimg = myimg[height_bot:height_top, width_left:width_right]
+        myimg = cv2.resize(myimg, (64, 64), interpolation=cv2.INTER_AREA)
+        data_npy_list.append(myimg)
+    img_npy = np.array(data_npy_list)
+    np.save(output_name, img_npy)
+    return levre_npy
 
-
-def crop_langue_images(filepath, longeur, output_name):
-    """
-
-    :param filepath:
-    :param longeur:
-    :param output_name:
-    :return:
-    """
-    pass
-    
 
 if __name__ == "__main__":
     img = mpimg.imread("../data_2021/ch1_en/levre/8081.bmp")
     print(img.shape)
     plt.figure()
     plt.imshow(img)
-    plt.show() 
-    
+    plt.show()
+
